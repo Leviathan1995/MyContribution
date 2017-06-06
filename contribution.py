579cf5ecd7e94485e7ee302bffd2daa4b9dd0c3e
7sDream <7seconddream@gmail.com>
2017-06-07 01:15:41 +0800

    format code

--- contribution.py
+++ contribution.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 
 basic_content = '''
 # MyContribution
+
 Crawl all merged pull request and show on `README.md`
 
 ## Basic
@@ -16,11 +17,17 @@ Crawl all merged pull request and show on `README.md`
  * BeautifulSoup
 
 #### How to use
+
 Fork this repository and 
         
-        python3 contribution.py
+```bash
+python3 contribution.py
+```
 
-## Contribution'''
+## Contribution
+
+'''
+
 
 class ContributionInfo:
     def __init__(self, pr_title, pr_url, project_name, project_url, star):
@@ -38,13 +45,17 @@ class Contribution:
         self.github_username = ""
         self.github_password = ""
         self.user_headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/48.0.2564.116 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;'
+                      'q=0.9,image/webp,*/*;q=0.8',
             'Accept-Encoding': 'gzip',
             'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'
         }
         self.session_url = 'https://github.com/session'
-        self.contribution_url = "https://github.com/pulls?q=is%3Apr+is%3Aclosed+is%3Amerged+author%3A"
+        self.contribution_url = "https://github.com/pulls?" \
+                                "q=is%3Apr+is%3Aclosed+is%3Amerged+author%3A"
         self.contribution_response = ""
         self.contribution_info = []
         self.session = ""
@@ -54,18 +65,23 @@ class Contribution:
         self.github_password = getpass.getpass("Your Github Password:")
         self.session = requests.Session()
         response = self.session.get(self.login_url, headers=self.user_headers)
-        pattern = re.compile(r'<input name="authenticity_token" type="hidden" value="(.*)" />')
+        pattern = re.compile(
+            r'<input name="authenticity_token" type="hidden" value="(.*)" />'
+        )
 
-        authenticity_token = pattern.findall(response.content.decode('utf-8'))[0]
+        response_text = response.content.decode('utf-8')
+        authenticity_token = pattern.findall(response_text)[0]
 
         login_data = {
             'commit': 'Sign in',
             'utf8': '%E2%9C%93',
-            'authenticity_token': authenticity_token, 'login': self.github_username,
+            'authenticity_token': authenticity_token,
+            'login': self.github_username,
             'password': self.github_password
         }
 
-        login_response = self.session.post(self.session_url, headers=self.user_headers, data=login_data)
+        login_response = self.session.post(
+            self.session_url, headers=self.user_headers, data=login_data)
         if str(login_response) == "<Response [200]>":
             print("Login success.")
         else:
@@ -77,10 +93,15 @@ class Contribution:
     def parser(self):
         print("Data crawling, Please wait a few minutes.")
         while self.contribution_url != "":
-            self.contribution_response = self.session.get(self.contribution_url, headers=self.user_headers)
-            soup = BeautifulSoup(self.contribution_response.text, 'html.parser')
+            self.contribution_response = self.session.get(
+                self.contribution_url, headers=self.user_headers
+            )
+            soup = BeautifulSoup(
+                self.contribution_response.text, 'html.parser'
+            )
 
-            pr_info = soup.find_all('a', 'link-gray-dark no-underline h4 js-navigation-open')
+            pr_info = soup.find_all(
+                'a', 'link-gray-dark no-underline h4 js-navigation-open')
             pr_project = soup.find_all('a', 'muted-link h4 pr-1')
             next_page = soup.find_all('a', 'next_page')
 
@@ -94,12 +115,21 @@ class Contribution:
                 project_url = project.a['href']
 
                 # Get star
-                project_response = self.session.get(project_url, headers=self.user_headers)
-                project_soup = BeautifulSoup(project_response.text, 'html.parser')
-                project_star = project_soup.find_all('a', 'social-count js-social-count')
+                project_response = self.session.get(
+                    project_url, headers=self.user_headers
+                )
+                project_soup = BeautifulSoup(
+                    project_response.text, 'html.parser'
+                )
+                project_star = project_soup.find_all(
+                    'a', 'social-count js-social-count'
+                )
                 star = BeautifulSoup(str(project_star[1]), 'html.parser')
 
-                c = ContributionInfo(pr_title.strip(), pr_url.strip(), project_name.strip(), project_url.strip(), star.string.strip())
+                c = ContributionInfo(
+                    pr_title.strip(), pr_url.strip(), project_name.strip(),
+                    project_url.strip(), star.string.strip()
+                )
                 self.contribution_info.append(c)
 
             if next_page:
@@ -108,12 +138,16 @@ class Contribution:
             else:
                 self.contribution_url = ""
 
-
     def write(self):
         print("Update README.md.")
-        str_info = '(***{} merged***)'.format(len(self.contribution_info)) + "\n"
+        str_info = '(***{} merged***)'.format(
+            len(self.contribution_info)
+        ) + "\n"
         for info in self.contribution_info:
-            str_info += ' * [**{}**(★{})]({}) - [{}]({})\n'.format(info.project_name, info.star, info.project_url, info.pr_title, info.pr_url)
+            str_info += ' * [**{}**(★{})]({}) - [{}]({})\n'.format(
+                info.project_name, info.star, info.project_url,
+                info.pr_title, info.pr_url
+            )
         str_contribution = basic_content + str_info
 
         if not os.path.isfile("README.md"):
@@ -124,11 +158,13 @@ class Contribution:
         file.write(str_contribution)
         file.close()
 
-    def push(self):
+    @staticmethod
+    def push():
         print("Push to Github.")
         os.system('git add README.md')
         os.system('git commit -m "update README.md"')
         os.system('git push')
+
 
 def main():
     c = Contribution()
