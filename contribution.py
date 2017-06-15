9e52980fa5d052acc7ceb8cda38fae5cdfcf6ffa
leviathan1995 <leviathan0992@gmail.com>
2017-06-15 11:24:41 +0800

    use subprocess.run()

--- contribution.py
+++ contribution.py
@@ -8,7 +8,7 @@ import json
 import re
 import time
 import warnings
-from subprocess import run
+import subprocess
 
 
 _BASE_CONTENT = '''# MyContribution
@@ -422,9 +422,9 @@ class ContributionsCrawler(object):
 
     def push(self):
         _step("Push to Github")
-        run(["git", "add", "README.md"])
-        run(["git", "commit", "-m", "'update README.md'"])
-        run(["git", "push"])
+        subprocess.run(["git", "add", "README.md"])
+        subprocess.run(["git", "commit", "-m", "'update README.md'"])
+        subprocess.run(["git", "push"])
 
     async def run_and_write(self, template=None, filename='README.md'):
         self.write(await self.run(), template, filename)
